076205335c190e5e9ddbe60a530b459e34576d10
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-28 16:12:48 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    #raise params.inspect
     @user = User.find_by(name: params[:user][:name])
-    binding.pry
-    #raise params.inspect
     return head(:forbidden) unless @user.authenticate(params[:password])
     session[:user_id] = @user.id
     redirect_to '/home'
