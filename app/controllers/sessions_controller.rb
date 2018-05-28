e1704508aa4f5ea41b4f1dae7c06f7cb1a7ba75b
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-28 16:34:48 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,11 @@ class SessionsController < ApplicationController
 
   def create
     if !params[:user][:password].nil? && params[:user][:password] != ''
-    @user = User.find_by(name: params[:user][:name])
-    #return head(:forbidden) unless @user.authenticate(params[:password])
-    session[:password] = @user.password
-    redirect_to '/home'
+      @user = User.find_by(name: params[:user][:name])
+      #return head(:forbidden) unless @user.authenticate(params[:password])
+      session[:password] = @user.password
+      redirect_to '/home'
+    else
+      redirect_to '/login'
   end
 end
